574079749ca20ce9a195d26a457964a3f7db5f79
Ivan Zhadaev <ivan.zhadaev@gmail.com>
2016-11-12 00:47:48 +0600

    Added test input component

--- js/app.js
+++ js/app.js
@@ -17,20 +17,60 @@ news = [
 	}
 ];
 
-var Article =  React.createClass({
-	getInitialState() {
+var TestInput = React.createClass({
+	getInitialState: function(){
 		return {
-			visible: false
+			inputValue: ''
 		}
+	},
+
+	changeInput: function(e){
+		this.setState({inputValue: e.target.value});
 	},
 
 	render: function(){
 		return (
 			<div>
-				<h3 className="news-item__title">{this.props.data.title}</h3>
-				<div className="news-item__preview">{this.props.data.preview}</div>
-				<a href="#" className="news-item__more">Подробнее</a>
-				<div className="news-item__content">{this.props.data.content}</div>
+				<input 
+					type="text" 
+					onChange={this.changeInput} 
+					value={this.state.inputValue} 
+					placeholder="Input some text..." 
+				/>
+				<p>{this.state.inputValue}</p>	
+			</div>	
+		)
+	}
+});
+
+var Article =  React.createClass({
+	getInitialState: function(){
+		return {
+			visible: false
+		}
+	},
+
+	showNewsContent: function(e){
+		e.preventDefault();
+		this.setState({visible: true}, function(){
+			console.log('visible state changed'); // callback function
+		});
+	},
+
+	render: function(){
+		var title   = this.props.data.title,
+			preview = this.props.data.preview,
+			content = this.props.data.content,
+			visible = this.state.visible;
+
+		//console.log('render', this);	
+
+		return (
+			<div>
+				<h3 className="news-item__title">{title}</h3>
+				<div className="news-item__preview">{preview}</div>
+				<a href="#" className={"news-item__more" + (visible ? ' hidden' : '')} onClick={this.showNewsContent}>Подробнее</a>
+				<div className={"news-item__content" + (visible ? '' : ' hidden')}>{content}</div>
 			</div>
 		);
 	}
@@ -40,6 +80,18 @@ var News = React.createClass({
 	propTypes: {
 		data: React.PropTypes.array.isRequired
 	},
+
+	getInitialState: function(){
+		return {
+			counter: 0
+		}
+	},
+
+	incrementCounter: function(e){
+		e.preventDefault();
+		this.setState({counter: ++this.state.counter});
+	},
+
 	render: function(){
 		var data = this.props.data,
 		newsItems;
@@ -58,10 +110,12 @@ var News = React.createClass({
 
 		return (
 			<div className="news-wrapper">
-			<h2 className="news-header">News block header</h2>
-				{newsItems}
-			</div>
-
+				<h2 className="news-header">News block header</h2>
+					{newsItems}
+				<hr/>
+				<div className="counter-wrapper">Now counter equals to {this.state.counter}</div>
+				<button onClick={this.incrementCounter}>+1</button>
+			</div>	
 		);
 	}
 });
@@ -71,6 +125,8 @@ var App = React.createClass({
 		return (
 			<div className="app-wrapper">
 				<h1 className="app-header">App header</h1>
+				<hr/>
+				<TestInput />
 				<hr/>
 				<News data={news}/>
 				<hr/>
